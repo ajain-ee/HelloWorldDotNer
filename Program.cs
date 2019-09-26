3c3ab3e8a550abb0491c606733622bd76fa39c54
anuj-jain <ajain@equalexperts.com>
2019-09-26 22:56:27 +0530

    Basic Hello World Console

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHi whats you name");
+            var name = Console.ReadLine();
+            Console.WriteLine($" Nice to meet you {name}");
+        }
+    }
+}
